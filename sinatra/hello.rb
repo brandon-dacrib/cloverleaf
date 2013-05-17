3da868531814520dae46b8b61def9d6f54c09df4
Brandon <brandon@dacrib.net>
2013-05-17 14:42:35 -0400

    initial attempt at getting the ruby to request a url on recieving a request
    Seems to be working, decided to use httparty over the native net/http
    library … cuz its nicer

--- sinatra/hello.rb
+++ sinatra/hello.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
+require	'httparty'
 
 get '/hello' do
   'Hello World'
+end
+
+get '/lighton' do
+  response = HTTParty.get('http://dacrib.net/lighton/')
+  puts response.body, response.code, response.message, response.headers.inspect
 end
