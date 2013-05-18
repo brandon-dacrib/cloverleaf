4b900ad475d76fdf65d9acdc7d208638254a8ed8
Brandon <brandon@dacrib.net>
2013-05-18 15:31:41 -0400

    updated path

--- sinatra/main.rb
+++ sinatra/main.rb
@@ -8,7 +8,7 @@ get '/hello' do
 end
 
 get '/light-on' do
-  response = HTTParty.get('http://dacrib.net/lighton')
+  response = HTTParty.get('http://dacrib.net/cloverleaf/test/lighton')
   puts response.body, response.code, response.message, response.headers.inspect
   @output = response.code, response.body
   "#{@output}"
