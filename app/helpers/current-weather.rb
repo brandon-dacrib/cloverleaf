4c18d3f8993cc9c5a8bf8b4402a19fef1f3e12fd
Brandon <brandon@dacrib.net>
2013-05-23 21:55:52 -0400

    Revert "trying it with a different module"
    This reverts commit bc3d57f441a0dfbfd1d050b6f736d4fca0f6343b.

--- app/helpers/current-weather.rb
+++ app/helpers/current-weather.rb
@@ -1,8 +1,15 @@
-#credit goes to this guy https://github.com/attack/barometer
+#credit goes to this guy https://github.com/stewart/weather-api
 require 'rubygems'
-require 'barometer'
+require 'weather-api'
 
-barometer = Barometer.new('New York')
-weather = barometer.measure
+# look up WOEID via http://weather.yahoo.com; enter location by city
+# name or zip and WOEID is at end of resulting page url.
 
-puts weather.current.temperature
+#make this a var based on zip code in a yaml file
+response = Weather.lookup(12761374)
+
+print <<EOT
+#{response.title}
+#{response.condition.temp} degrees
+#{response.condition.text}
+EOT
