38ee1afb934c1aad9e0eeb95713356a294cc3277
Brandon <brandon@dacrib.net>
2013-06-02 16:14:09 -0400

    got api calls using the nodeid to properly pass to the ISY with an authenticated http request: sample get
    http://localhost:4567/do/20%20BC%2093%201/0 -- toggles light on
    http://localhost:4567/do/20%20BC%2093%201/0 -- toggles light off
    
    20%20BC%2093%201 is the url encoded insteon nodeid +1 @ the end

--- app/routes/api/do.rb
+++ app/routes/api/do.rb
@@ -1,16 +1,25 @@
 #insteon devices attached to an ISY99 have a resful API documented here: #http://wiki.universal-devices.com/index.php?title=ISY-99i_Series_INSTEON:REST_Interface
 #for those we really just need to translate the URL, samples below
+require 'uri'
 
-get '/do/*/1' do
-  response = HTTParty.get('http://dacrib.net/cloverleaf/test/lighton')
-  puts response.body, response.code, response.message, response.headers.inspect
-  @output = response.code, response.body
+class ISY
+  include HTTParty
+  base_uri '192.168.1.204'
+  basic_auth 'admin', 'admin'
+end
+
+get '/do/:nodeid/1' do
+  node = URI.encode("#{params[:nodeid]}")
+  response = ISY.get("/rest/nodes/#{node}/cmd/DON")
+  puts response.code, response.body, response.message, response.headers.inspect
+  @output = response.message
   "#{@output}"
 end
 
 get '/do/:nodeid/0' do
-  response = HTTParty.get('http://192.168.1.204/rest/nodes/#{params[:nodeid]}/cmd/DOF')
-  puts response.body, response.code, response.message, response.headers.inspect
-  @output = response.code, response.body
+  node = URI.encode("#{params[:nodeid]}")
+  response = ISY.get("/rest/nodes/#{node}/cmd/DOF")
+  puts response.code, response.body, response.message, response.headers.inspect
+  @output = response.message
   "#{@output}"
 end
