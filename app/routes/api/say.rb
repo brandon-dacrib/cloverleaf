eaca1a15ff79ee226a358875492831b8782c81e4
Brandon <brandon@dacrib.net>
2013-06-03 17:27:51 -0400

    fixed spacing issue

--- app/routes/api/say.rb
+++ app/routes/api/say.rb
@@ -1,12 +1,14 @@
 # cross platform (os x/linux text to speech, uses the google translate api)
 require 'rubygems'
 require 'sinatra'
+require 'uri'
 
 #speech.sh script borrowed from:
 #http://danfountain.com/2013/03/raspberry-pi-text-to-speech/
 
 get '/say/:utterance' do
-	system("helpers/speech.sh #{params[:utterance]}")
+  utter = URI.decode("#{params[:utterance]}")
+	system("helpers/say.rb /'#{utter}/'")
 	@output = 'OK'
 	"#{@output}"
 end
