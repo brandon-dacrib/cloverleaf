73c30e8256f4ab5e322c6803ba60e58762271cf1
Brandon <brandon@dacrib.net>
2013-06-05 21:31:23 -0400

    changes

--- app/helpers/notify-growl.rb
+++ app/helpers/notify-growl.rb
@@ -2,25 +2,25 @@ require 'rubygems'
 require 'ruby_gntp'
 
 # -- Standard way
-growl = GNTP.new("Ruby/GNTP self test")
+growl = GNTP.new("Cloverleaf Notification")
 growl.register({:notifications => [{
-  :name     => "notify",
+  :name     => "cloverleaf",
   :enabled  => true,
 }]})
 
 growl.notify({
-  :name  => "notify",
-  :title => "Congraturation",
-  :text  => "Congraturation! You are successful install ruby_gntp.",
+  :name  => "cloverleaf",
+  :title => "Cloverleaf Title",
+  :text  => "X was turned on",
   :icon  => "http://www.hatena.ne.jp/users/sn/snaka72/profile.gif",
   :sticky=> true,
 })
 
 # -- Instant notification
-GNTP.notify({
-  :app_name => "Instant notify",
-  :title    => "Instant notification", 
-  :text     => "Instant notification available now.",
-  :icon     => "http://www.hatena.ne.jp/users/sn/snaka72/profile.gif",
-})
+#GNTP.notify({
+#  :app_name => "Instant notify",
+#  :title    => "Instant notification", 
+#  :text     => "Instant notification available now.",
+#  :icon     => "http://www.hatena.ne.jp/users/sn/snaka72/profile.gif",
+#})
 
