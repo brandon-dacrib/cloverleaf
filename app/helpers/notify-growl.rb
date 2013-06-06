1ac3f82556396cfb73d2290c01995a7be8fa1bd8
Brandon <brandon@dacrib.net>
2013-06-05 21:18:25 -0400

    sample script to send notification to growl

--- app/helpers/notify-growl.rb
+++ app/helpers/notify-growl.rb
@@ -1,5 +1,26 @@
 require 'rubygems'
-require 'ruby-growl'
- 
-g = Growl.new("192.168.1.101", "ruby-growl", ["ruby-growl Notification"], ["ruby-growl Notification"], nil)
-g.notify("ruby-growl Notification", "It Came From Ruby-Growl", "Greetings!", 1, true)
+require 'ruby_gntp'
+
+# -- Standard way
+growl = GNTP.new("Ruby/GNTP self test")
+growl.register({:notifications => [{
+  :name     => "notify",
+  :enabled  => true,
+}]})
+
+growl.notify({
+  :name  => "notify",
+  :title => "Congraturation",
+  :text  => "Congraturation! You are successful install ruby_gntp.",
+  :icon  => "http://www.hatena.ne.jp/users/sn/snaka72/profile.gif",
+  :sticky=> true,
+})
+
+# -- Instant notification
+GNTP.notify({
+  :app_name => "Instant notify",
+  :title    => "Instant notification", 
+  :text     => "Instant notification available now.",
+  :icon     => "http://www.hatena.ne.jp/users/sn/snaka72/profile.gif",
+})
+
