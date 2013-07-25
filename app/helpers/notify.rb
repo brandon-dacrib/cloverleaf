286edea9aa8d7ba8f225811c5e1e609be9eb76fb
Brandon <brandon@dacrib.net>
2013-07-24 21:19:24 -0400

    adding initial support for growl notifications

--- app/helpers/notify.rb
+++ app/helpers/notify.rb
@@ -4,14 +4,19 @@ case ARGV[0]
 when "email"
   #email
   
-when "growl"
-  #growl broadcast
+when "growl" #growl notifications
+  hostname = ARGV[1]
+  message  = ARGV[2]
+  system("growl -n "cloverleaf says" -H #{hostname} -m #{message}")
+  
+when "say"
+  #basically say with addressing vs broadcast to all
   
 when "sms"
   #sms
   
-when "say"
-  #basically say with addressing vs broadcast to all
+when "all"
+  #broadcast something really important
   
 else
   puts "usage: notify.rb [method] [address] [message] \n" 
