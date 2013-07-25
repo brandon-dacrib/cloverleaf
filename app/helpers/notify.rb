2294ad99a2aaa9aa0ad530e3525e6463e04fcf18
Brandon <brandon@dacrib.net>
2013-07-25 19:49:12 -0400

    another try

--- app/helpers/notify.rb
+++ app/helpers/notify.rb
@@ -5,7 +5,7 @@ when "email"      #email something
   require 'parseconfig'
   require 'pony'
   config = ParseConfig.new('config/cloverleaf.conf')
-  
+  puts config.get_value('from_email')
   address = ARGV[1]
   message = ARGV[2]
   Pony.mail(:to => address, :from => from_email, :subject => 'cloverleaf says:', :body => message)
