409c4988d3213a1927f732198c71880e5fd7aa06
Brandon <brandon@dacrib.net>
2013-09-06 14:39:51 -0400

    cleanups and renames

--- app/tests/openweathermap-test.rb
+++ app/tests/openweathermap-test.rb
@@ -1,13 +1,12 @@
 require 'rubygems'
 require 'httparty'
-require 'json'
  
  
-class Forecast
+class CurrentWeather
   include HTTParty
  
   def initialize
-    @forecast = self.class.get('http://api.openweathermap.org/data/2.5/weather?lat=40&lon=-74&units=imperial', :output => :json)
+    @weatherdata = self.class.get('http://api.openweathermap.org/data/2.5/weather?lat=40&lon=-74&units=imperial', :output => :json)
   end
  
   def description
@@ -25,13 +24,13 @@ class Forecast
   private
  
   def weather
-    @forecast['weather'].first
+    @weatherdata['weather'].first
   end
  
   def main
-    @forecast['main']
+    @weatherdata['main']
   end
 end
  
-forecast = Forecast.new
-puts "current weather: #{forecast.description}, temparature is #{forecast.temp} degrees, humidity is at #{forecast.humidity} percent"
+weatherdata = CurrentWeather.new
+puts "current weather: #{weatherdata.description}, temparature is #{weatherdata.temp} degrees, humidity is at #{weatherdata.humidity} percent"
