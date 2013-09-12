5248313d68dd52f3b28734e33b4b7f0f8b33214b
Brandon <brandon@dacrib.net>
2013-09-12 17:20:21 -0400

    initial add of forecast argument
    going to have to decide what elements I want out of the forecast + how
    to pull and use them.

--- app/helpers/wunderground.rb
+++ app/helpers/wunderground.rb
@@ -1,25 +1,33 @@
 #!/usr/local/bin/ruby
 require 'rest-client'
 require 'json'
- 
- case ARGV[0]
+require 'parseconfig'
+require 'awesome_print'
+  
+conf = ParseConfig.new('config/cloverleaf.conf')
+api_key = conf['wunderground_api_key']
+zip = conf['wunderground_zip']
+
+case ARGV[0]
+
 when "current"      #get current weather
-  require 'parseconfig'
-  conf = ParseConfig.new('config/cloverleaf.conf')
-  api_key = conf['wunderground_api_key']
-  zip = conf['wunderground_zip']
+	url = "http://api.wunderground.com/api/#{api_key}/geolookup/conditions/q/#{zip}.json"
+	res = RestClient.get url
+	parsed_json = JSON.parse(res)
+	location = parsed_json['location']['city']
+	weather = parsed_json['current_observation']['weather']
+	temp_f = parsed_json['current_observation']['temp_f']
+	relative_humidity = parsed_json['current_observation']['relative_humidity']
+	feelslike_f = parsed_json['current_observation']['feelslike_f']
 
-  url = "http://api.wunderground.com/api/#{api_key}/geolookup/conditions/q/#{zip}.json"
+	puts "current weather: #{weather}, temperature is #{temp_f} degrees, relative_humidity is #{relative_humidity}, and it feels like it is #{feelslike_f} degrees.\n"
 
-  res = RestClient.get url
-  parsed_json = JSON.parse(res)
-  location = parsed_json['location']['city']
-  weather = parsed_json['current_observation']['weather']
-  temp_f = parsed_json['current_observation']['temp_f']
-  relative_humidity = parsed_json['current_observation']['relative_humidity']
-  feelslike_f = parsed_json['current_observation']['feelslike_f']
-
-  print "current weather: #{weather}, temperature is #{temp_f} degrees, relative_humidity is #{relative_humidity}, and it feels like it is #{feelslike_f} degrees.\n"
+when "forecast"
+	url = "http://api.wunderground.com/api/#{api_key}/geolookup/forecast/q/#{zip}.json"
+	res = RestClient.get url
+	parsed_json = JSON.parse(res)
+	#ap parsed_json
+	puts parsed_json[0]
 
 else
 	puts "usage: wunderground.rb [method]\n" 
