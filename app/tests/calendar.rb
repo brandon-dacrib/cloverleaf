fece6f2cf5a4a69768a762e327d82a1d45740d7a
Brandon <brandon@dacrib.net>
2013-09-16 17:15:45 -0400

    the start of some code to check the google calander and say reminders

---
+++ app/tests/calendar.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'google_calendar'
+require 'parseconfig'
+
+conf = ParseConfig.new('config/cloverleaf.conf')
+username = conf['google_services_user']
+password = conf['google_services_pass']
+
+cal = Google::Calendar.new(:username => username,
+                           :password => password,
+                           :app_name => 'cloverleaf-googlecalendar-integration')
+
+puts cal.find_events('Housework')
