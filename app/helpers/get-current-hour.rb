e5cc87995275cb8b67037585a768721c4c4d778f
Brandon <brandon@dacrib.net>
2013-10-17 20:09:27 -0400

    fixing this, it somehow got fucked up

--- app/helpers/get-current-hour.rb
+++ app/helpers/get-current-hour.rb
@@ -4,3 +4,4 @@ cwd = Dir.pwd
 
 system("wget -O /dev/null http://localhost:4567/say/the%20time%20%is%now%20#{currenthour}")
 #system("echo rm -f #{cwd}/aplay-D_card0-t_raw-f_S16_LE-r48000-c_2.raw")
+#echo added empty line
