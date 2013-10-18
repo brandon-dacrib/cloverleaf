bc2bd107ddf848a27ce13b82be5b2b673fee7d5c
Brandon <brandon@dacrib.net>
2013-10-17 20:20:55 -0400

    + -s for silent

--- app/helpers/get-current-hour.rb
+++ app/helpers/get-current-hour.rb
@@ -2,5 +2,5 @@
 currenthour = Time.now.strftime("%-l%p")
 cwd = Dir.pwd
 
-system("curl -q -o /dev/null http://localhost:4567/say/the%20time%20is%20now%20#{currenthour}")
+system("curl -s -o /dev/null http://localhost:4567/say/the%20time%20is%20now%20#{currenthour}")
 #system("echo rm -f #{cwd}/aplay-D_card0-t_raw-f_S16_LE-r48000-c_2.raw")
