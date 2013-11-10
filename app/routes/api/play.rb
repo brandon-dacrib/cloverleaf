bc819414889b70772e61d7895b2af51cd982b39a
Brandon <brandon@dacrib.net>
2013-11-10 17:51:54 -0500

    fix helper name

--- app/routes/api/play.rb
+++ app/routes/api/play.rb
@@ -5,7 +5,7 @@ require 'uri'
 
 get '/play/:file' do
   utter = URI.decode("#{params[:file]}")
-	system("helpers/play-audio.rb #{file}")
+	system("helpers/play.rb #{file}")
 	@output = 'OK'
 	"#{@output}"
 end
