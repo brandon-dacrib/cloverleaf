f1dc26229a4b826fd750e02fbd3f8f2769047971
Brandon Price <brandon@dacrib.net>
2014-01-13 20:48:14 -0500

    Included the google_tts hack for outputting strings over 100 characters https://github.com/charlierevett/google_tts

--- app/helpers/say.rb
+++ app/helpers/say.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/ruby
 require 'rubygems'
-require 'tts'
-Tts.server_url  "http://tts-api.com/tts.mp3"
+require_relative '../lib/google_tts'
+#require 'tts'
+#Tts.server_url  "http://tts-api.com/tts.mp3"
 
 input = ARGV
 #	puts "#{input}"
   
-"#{input}".to_file "en", "/tmp/utterance.mp3"
+"#{input}".to_mp3 "/tmp/utterance.mp3"
 system("mpg123 -q /tmp/utterance.mp3")
 system("rm -f /tmp/utterance.mp3")
