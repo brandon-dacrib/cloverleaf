3768d7d822476f402f83c73a2a19e81c99a5d75b
Brandon <brandon@dacrib.net>
2014-01-18 18:09:58 -0500

    added test .js file modified lights.erb to use a drop down for dimming still need to make that drop down actually work

---
+++ app/public/js/test.js
@@ -0,0 +1,11 @@
+$('#alertMe').click(function(e) {
+    window.alert("alerted");
+   e.preventDefault();// prevent the default anchor functionality
+});
+
+//$('.dropdown-menu li').click(function(e){
+ //  e.preventDefault();
+  // var selected = $(this).text();
+   //$('.box').val(selected);
+
+//});
