6e35949da24988c7683cae0f0efe25bbd2bf96cc
Brandon Price <brandon@dacrib.net>
2014-01-21 15:00:44 -0500

    Trying to do a post w/ a slim template + cleanups

--- app/routes/webapp/main.rb
+++ app/routes/webapp/main.rb
@@ -9,4 +9,4 @@ get ('/blinds') { erb :blinds}
 get ('/lights') { erb :lights}
 get ('/sensors') {erb :sensors}
 #get ('/settings') { erb :settings}
-get ('/test') { erb :test}
+get ('/test') { slim :test}
