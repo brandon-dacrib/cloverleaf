7b00a1542afb982ee3f5cc822778af25dee08d94
Brandon <brandon@dacrib.net>
2014-01-26 22:05:12 -0500

    working on moving stuff in do.rb to pull data from the database

--- app/routes/api/do.rb
+++ app/routes/api/do.rb
@@ -1,22 +1,37 @@
 #insteon devices attached to an ISY99 have a resful API documented here: #http://wiki.universal-devices.com/index.php?title=ISY-99i_Series_INSTEON:REST_Interface
 #for those we really just need to translate the URL, samples below
 require 'uri'
-require 'parseconfig'
+require 'sequel'
+require 'logger'
+#require '/Users/brandon/Documents/devel/cloverleaf/app/routes/api/database.rb'
+
 
 class ISY
   include HTTParty
-  conf = ParseConfig.new('config/cloverleaf.conf')
-    @isy_uri = conf['isy_uri']
-    @isy_user = conf['isy_user']
-    @isy_pass = conf['isy_pass']
+  @keys = Keys
+    @isy_uri  = Keys.select(:value).where[:key => 'isy_uri']
+    @isy_user = Keys.select(:value).where[:key => 'isy_user']
+    @isy_pass = Keys.select(:value).where[:key => 'isy_pass']
+    print " uri: #{@isy_uri} user: #{@isy_user} pass: #{@isy_pass}"
 
   # Create bits for httparty
     base_uri "#{@isy_uri}"
     basic_auth "#{@isy_user}", "#{@isy_pass}"
 end
 
+class Device
+  @devices = Devices
+end
+
+class Router
+  @routers = Routers
+end
+
 get '/api/do/:nodeid/0/*' do
   node = URI.encode("#{params[:nodeid]}")
+  address = Devices.select(:device_address).where[:device_id => params[:nodeid]]
+  routerid = Devices.select(:device_routerid).where[:device_id => params[:nodeid]]
+  router_address = Routers.select(:router_address).where[:router_id => "#{routerid}"]
   response = ISY.get("/rest/nodes/#{node}/cmd/DFOF")
   puts response.code, response.body, response.message, response.headers.inspect
   @output = response.message
--- app/routes/webapp/main.rb
+++ app/routes/webapp/main.rb
@@ -1,12 +1,11 @@
 #sample sinatra
 require 'rubygems'
 require 'sinatra'
-require 'data_mapper'
+require 'sequel'
 require 'slim'
 
 get ('/') { erb :index}
 get ('/blinds') { erb :blinds}
 get ('/lights') { erb :lights}
 get ('/sensors') { erb :sensors}
-#get ('/settings') { erb :settings}
 get ('/test') { erb :test}
