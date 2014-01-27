9ad8146ebf46acc03366fd8be730293c3c520ceb
Brandon <brandon@dacrib.net>
2014-01-26 19:30:41 -0500

    added a script to create the db moved those bits out of webapp database.rb

---
+++ app/modules/create-db.rb
@@ -0,0 +1,30 @@
+require 'sequel'
+require 'logger'
+require 'sinatra/sequel'
+
+DB = Sequel.sqlite('data/sqlite/cloverleaf.db', :loggers => [Logger.new($stdout)]) 
+#DB = Sequel.sqlite('', :loggers => [Logger.new($stdout)])
+DB.create_table :routers do
+	primary_key :router_id
+	String	:router_address, :unique => true, :null => false
+	Text	:router_desc, :unique => true
+	Text	:router_auth_user
+	Text	:router_auth_pass
+   Integer	:router_port, :null => false
+   Text	:router_protocol, :null => false
+   Integer	:router_type, :null => false
+end
+
+DB.create_table :devices do
+	primary_key	:device_id
+	String	:device_address, :unique => true, :null => false
+	Text	:device_desc
+	String	:device_type
+	String	:device_routerid
+end
+
+DB.create_table	:keys do
+	primary_key	:key_id
+	Text	:key, :null => false
+	Text	:value, :null => false
+end
