9665377ead9a4179ed8d6b05fe00db11f4fab079
Brandon Price <brandon@dacrib.net>
2014-07-23 15:09:10 -0400

    updated some parameters

--- app/modules/autorip.sh
+++ app/modules/autorip.sh
@@ -5,7 +5,7 @@ outfile=`makemkvcon info -r disc:0 | grep BD | awk -F, '{print $6}' | sed 's/\"/
 tmpdir="/mnt/backup/tmp"
 echo $outfile
 echo "backing up and decrypting $outfile:"
-makemkvcon -r --decrypt --cache=1024 --minlength=3600 --progress=$tmpdir/movie.log backup disc:0 $tmpdir/$outfile
+makemkvcon -r --decrypt --cache=1024 --minlength=3600 --messages=$tmpdir/movie-messages.log --progress=$tmpdir/movie-progress.log backup disc:0 $tmpdir/$outfile
 echo "encoding $outfile:"
 HandBrakeCLI -i $tmpdir/$outfile/ --main-feature -s 1 -m -q 1 -o $tmpdir/$outfile.mkv
 
