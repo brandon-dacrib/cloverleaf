9adba782ea68d2d25c12d431fc60fddd7637a19e
Brandon Price <brandon@dacrib.net>
2014-07-23 21:36:23 -0400

    working example of this api actually working as a client to the ruby API good method of building out one feature at a time until all is ported over to nodejsx

--- app-js/app.js
+++ app-js/app.js
@@ -1,27 +1,63 @@
 var express = require('express');
 var app = express();
 
-app.get('/api/do/:nodeid/:action', function(req, res){
+
+app.get('/api/:call/:item?/:action?', function(req, res){
   var url = req.url;
-  var nodeid = req.params.nodeid;
+  var call = req.params.call;
+  var item = req.params.item;
   var action = req.params.action;
-  res.send("url: " + url + "\nnodeid: " + nodeid + "\naction: " + action);
+  res.send("url: " + url + "\nitem: " + item + "\naction: " + action);
 
-var Client = require('node-rest-client').Client;
+switch(call) {
+	case 'do':
+	console.log ("the call is do");
+	console.log ("item is: "+ item);
+	console.log ("action is: "+ action);
+	console.log ("request url is: "+ req.url);
 
-client = new Client();
+	var Client = require('node-rest-client').Client;
+	client = new Client();
+	client.get("http://cloverleaf/api/" + call  + "/" + item + "/" + action + "/", function(data, response){
+		//res.send(data);
+		//console.log(response);
+	});
+
+	break;
+
+	case 'say':
+	console.log ("the call is say");
+	console.log ("the item is "+ item);
+	console.log ("request url is: "+ req.url);
+	break;
+
+	case 'play':
+	console.log ("the call is play");
+	console.log ("the item is "+ item);
+	console.log ("the request url is:"+ req.url);
+	break;
+
+	case 'notify':
+	console.log ("the call is notify");
+	console.log ("the item is "+ item);
+	console.log ("the request url is:"+ req.url);
+
+}
+
+//var Client = require('node-rest-client').Client;
+//client = new Client();
 
 //direct way
-client.get("http://cloverleaf/" + url, function(data, response){
+//client.get("http://cloverleaf/" + url, function(data, response){
 	//parsed response body as js object
-	console.log(data);
+	//console.log(data);
 	//raw response
 	//res.send(data);
 	//console.log(response);
-	console.log ("request url is: "+ req.url);
-	console.log ("nodeid is: "+ nodeid);
-	console.log ("action is: "+ action);
-});
+//	console.log ("request url is: "+ req.url);
+//	console.log ("item is: "+ item);
+//	console.log ("action is: "+ action);
+//});
 
 });
 
