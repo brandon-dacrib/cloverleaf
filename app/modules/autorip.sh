3b6487c00bd7efb405c3969f7c1ec5acb2b0f4ae
brandon <brandon@dacrib.net>
2014-08-25 15:46:27 -0400

    .sqv support
    added support for the case that .sqv need to be downloaded

--- app/modules/autorip.sh
+++ app/modules/autorip.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #outfile=$1
 echo "getting movie title:"
-outfile=`makemkvcon info -r disc:0 | grep BD | awk -F, '{print $6}' | sed 's/\"//g'`
+outfile=`makemkvcon info -r disc:0 | grep BD | awk -F, '{print $6}' | sed 's/\"//g' | sed 's/ /-/g' | head -n 1`
 tmpdir="/mnt/backup/tmp"
 echo $outfile
 echo "backing up and decrypting $outfile:"
-makemkvcon -r --decrypt --cache=1024 --minlength=3600 --messages=$tmpdir/movie-messages.log --progress=$tmpdir/movie-progress.log backup disc:0 $tmpdir/$outfile
+echo "makemkvcon --minlength=1800 -r --decrypt --cache=2048 --progress=$tmpdir/movie.log backup disc:0 $tmpdir/$outfile"
+makemkvcon --minlength=1800 -r --decrypt --cache=2048 --progress=$tmpdir/movie.log backup disc:0 $tmpdir/$outfile
 echo "encoding $outfile:"
 HandBrakeCLI -i $tmpdir/$outfile/ --main-feature -s 1 -m -q 1 -o $tmpdir/$outfile.mkv
 
